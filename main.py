6f38989c2a483e692be64541131a4b0434eae282
Local <76016636+LocalsGitHub@users.noreply.github.com>
2021-12-08 11:49:56 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# Grade Point Average Calculator
+
+class calculator():
+    
+    def __init__(self):
+        
+        self.class1 = input('Enter Math Grade: ')
+        self.class2 = input('Enter Elective Grade: ')
+        self.class3 = input('Enter P.E Grade: ')
+        self.class4 = input('Enter English Grade: ')
+        self.class5 = input('Enter Science Grade: ')
+        self.class6 = input('Enter Social Studies Grade: ')
+
+        self.result = self.calculate(self.class1, self.class2, self.class3, self.class4, self.class5, self.class6)
+        
+        
+    def calculate(self, class1, class2, class3, class4, class5, class6):
+        newClasses = []
+        classes = [class1, class2, class3, class4, class5, class6]
+        for class1 in classes:
+            if class1.isdigit():
+                if int(class1) == 100:
+                    newClasses.append("A")
+                elif int(class1) < 100:
+                    newClasses.append("A")
+                elif int(class1) < 90:
+                    newClasses.append("A")
+                elif int(class1) < 80:
+                    newClasses.append("B")
+                elif int(class1) < 70:
+                    newClasses.append("C")
+                elif int(class1) < 60:
+                    newClasses.append("D")
+                elif int(class1) < 50:
+                    newClasses.append("F")
+            else:
+                newClasses.append(class1)
+        convert = {'A': 4.0, 'B': 3.0, 'C': 2.0, 'D': 1.0, 'F': 0.0}
+        for i in newClasses:
+            if i not in convert:
+                print('Error: Invalid Grade')
+                return
+        return (convert[newClasses[0]] + convert[newClasses[1]] + convert[newClasses[2]] + convert[newClasses[3]] + convert[newClasses[4]] + convert[newClasses[5]]) / 6
+    
+    def print_result(self) -> str:
+        print('\nYour GPA is: ' + str(round(float(self.result), 1)))
+
+Calc = calculator()
+Calc.print_result()
